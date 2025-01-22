477c5d57e4b2cdf58908dbaf5563cb9e2b6c3e39
daromi94 <davromero07@gmail.com>
2025-01-21 20:15:07 -0500

    feat: :sparkles: use concurrent map for lru cache

--- src/core/cache/src/main/java/com/daromi/stash/core/cache/Cache.java
+++ src/core/cache/src/main/java/com/daromi/stash/core/cache/Cache.java
@@ -1,10 +1,17 @@
 package com.daromi.stash.core.cache;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public interface Cache<K, V> {
 
   Optional<V> get(final K key);
 
+  V getIfPresent(final K key);
+
+  V getOrElse(final K key, final Supplier<? extends V> supplier);
+
   void put(final K key, final V value);
+
+  void invalidate(final K key);
 }
--- src/core/cache/src/main/java/com/daromi/stash/core/cache/LruCache.java
+++ src/core/cache/src/main/java/com/daromi/stash/core/cache/LruCache.java
@@ -2,18 +2,40 @@ package com.daromi.stash.core.cache;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Supplier;
 
 public final class LruCache<K, V> implements Cache<K, V> {
 
-  private LruCache() {}
+  private final ConcurrentMap<K, V> store;
+
+  private LruCache() {
+    this.store = new ConcurrentHashMap<>();
+  }
 
   @Override
   public Optional<V> get(final K key) {
     Objects.requireNonNull(key, "key must not be null");
 
-    // TODO
+    final var value = store.get(key);
 
-    return Optional.empty();
+    return Optional.ofNullable(value);
+  }
+
+  @Override
+  public V getIfPresent(final K key) {
+    Objects.requireNonNull(key, "key must not be null");
+
+    return store.get(key);
+  }
+
+  @Override
+  public V getOrElse(final K key, final Supplier<? extends V> supplier) {
+    Objects.requireNonNull(key, "key must not be null");
+    Objects.requireNonNull(supplier, "supplier must not be null");
+
+    return store.containsKey(key) ? store.get(key) : supplier.get();
   }
 
   @Override
@@ -21,6 +43,13 @@ public final class LruCache<K, V> implements Cache<K, V> {
     Objects.requireNonNull(key, "key must not be null");
     Objects.requireNonNull(value, "value must not be null");
 
-    // TODO
+    store.put(key, value);
+  }
+
+  @Override
+  public void invalidate(final K key) {
+    Objects.requireNonNull(key, "key must not be null");
+
+    store.remove(key);
   }
 }
